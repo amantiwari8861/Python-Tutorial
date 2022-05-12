1507433526074bf951db97b87e6e777d4e83c5d5
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-05-12 18:21:53 +0530

    operators,input

---
+++ secondclass.py
@@ -0,0 +1,42 @@
+# import keyword
+#variable defining rules
+# num ber=10  #blanks not allowed
+# print(num ber)
+# 1num=30  #numeric at first letter not allowed
+# print(1num)
+# num$=70 #no special characters allowed except underscore(_)
+# print(num$)
+# num_one=40
+# print(num_one)
+
+# for=50 #no pre-defined keywords allowed
+# print(for)
+
+# list of keywords
+# print(keyword.kwlist)
+
+# ----------------------------------------------------------------
+#identity operators
+# name="Aman Tiwari"
+# name2="Aman Tiwari"
+# print(name is name2)
+# print(name is not name2)
+
+# membership operators
+# name3="hello everyone"
+# print('no' in name3)
+# print('hello' not in name3)
+
+#ternary operator (conditional operator)
+
+# num=int(input("enter num1 \n")) #\n for new line
+# num=int(input("enter num1 \n")) #\n for new line
+# num2=int(input("enter num2 \t")) #\t for tabular space (tab)
+
+# min=num if num<num2 else num2
+# print("the minimun is ",min)
+# max=num if num>num2 else num2
+# print("the maximum is ",max)
+
+print("hello",end="\t")
+print("h","e","l",sep="@")
---
+++ volofsphere.py
@@ -0,0 +1,14 @@
+''' num1="10"
+num2="20"
+print(num1+num2)
+ '''
+# volume of sphere = 4/3 * pi * r^3
+pi=3.14
+radius=float(input("Enter radius: "))
+# print(type(radius))
+volume=4/3 * pi * radius**3
+print("the volume of sphere is ",volume)
+# print(pi,"the volume of sphere is ",volume," of radius ",radius)
+
+
+
