78460b93ef4ddc4e86fa0d770d4ff6485ac9c60c
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-07-17 14:12:09 +0530

    added dictionary

---
+++ 04 Collections/Dictionary.py
@@ -0,0 +1,61 @@
+# user_details={"name":"Aman","addhar":123456,5:"Five"}
+
+# print("the user details is :",user_details)
+
+# print(user_details.get("name"))
+# print(user_details.get("5"))
+# print(user_details.get(5))
+
+# print(user_details.keys())
+# print(user_details.values())
+# print(user_details.items())
+
+# userdict=user_details.copy()
+# print(userdict)
+
+# user_details.update({"name":"Aman Tiwari"})
+
+# print(user_details)
+
+# user_details.update({"Age":23})
+# print(user_details)
+
+# print("aadhar :",user_details["addhar"])
+
+# user_details.pop(5)
+
+# print(user_details)
+
+# user_details.popitem()
+# print(user_details)
+# user_details.popitem()
+# print(user_details)
+
+# del user_details["name"]
+# print(user_details)
+
+# print(user_details)
+# del user_details
+# user_details.clear()
+# print(user_details)
+
+
+# for e in user_details:
+    # print(e) # for key
+    # print(user_details[e]) # for value
+    
+
+# for k,v in user_details.items():
+#     print("key :",k," value :",v)
+
+user_details={
+                "name":"Aman",
+                "addhar":123456,
+                "address":{
+                            "state":"UP",
+                            "city":"noida",
+                            "pincode":201301
+                          }
+            }
+print(user_details)
+print(user_details["address"]["city"])
--- 04 Collections/set.py
+++ 04 Collections/set.py
@@ -14,11 +14,31 @@ fruits={"Apple","Mango","Apple","Banana",10,78,54.55}
 
 # fruits.discard("Banana")
 # fruits.remove("Banana")
-print(fruits)
+# print(fruits)
 
-fruits.update(["custordapple","cherry"])
-print(fruits)
+# fruits.update(["custordapple","cherry"])
+# print(fruits)
 
-fruits.difference_update(["custordapple","cherry","kashmiri apple"])
-print(fruits)
+# fruits.difference_update(["custordapple","cherry","kashmiri apple"])
+print("All fruits :",fruits)
+
+favourite_fuits={"Apple","Mango","litchi"}
+print('favourite fruits :',favourite_fuits)
+vegetables={"potato","spinach"}
+# print("after difference :",fruits.difference(favourite_fuits))
+# print("after intersection :",fruits.intersection(favourite_fuits))
+# print("union :",fruits.union(favourite_fuits))
+# print(" is disjoint :",fruits.isdisjoint(favourite_fuits))
+# print(" is disjoint :",fruits.isdisjoint(vegetables))
+
+
+A={1,2,3,4,5}  
+B={1,2,3}
+C={1,2,3,8,9}
+print(A.issubset(B)) # is all Element of A is present in B
+print(A.issuperset(B)) # is All elements of B present in A
+
+print(B.issubset(A))
+
+print(A.symmetric_difference(C))
 
