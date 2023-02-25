f1db0745bd39ec13e847d00efbf770704066a57d
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-25 16:24:08 +0530

    removed pdfs

--- 02 variables_and_DataTypes/volofsphere.py
+++ 02 variables_and_DataTypes/volofsphere.py
@@ -1,7 +1,6 @@
 # volume of sphere = 4/3 * pi * r^3
 pi=3.14
 radius=float(input("Enter radius: "))
-# print(type(radius))
 volume=4/3 * pi * radius**3
 print("the volume of sphere is ",volume)
 # print(pi,"the volume of sphere is ",volume," of radius ",radius)
