136a4d7b330066f94233fdcf0a02fa78b33c872b
Aman Tiwari <amantiwari2743@gmail.com>
2023-06-20 13:15:19 +0530

    minor description added

--- 01 intro_to_python/escapeSequence.py
+++ 01 intro_to_python/escapeSequence.py
@@ -1,10 +1,11 @@
 # print("hello Aman sir")
+# print("hello Aman sir")
 # print("hello\nworld") # \n means new line
 # print("hello\tworld") # \t means tabular space
 
-# print("hello "Aman" sir") #error
-# print("hello \"Aman\" sir")
 # print("hello 'aman' sir")
+# print("hello "Aman" sir") #error
+# print("hello \"Aman\" sir") #putting double quote in double quote
 
 # print('heloo aman sir')
 # print('heloo 'aman' sir') #error
@@ -20,10 +21,10 @@
 # print("hello Aman Sir")
 # print("how are you")
 
-# print("hello Aman Sir ",end="")
+# print("hello Aman Sir",end=" ")
 # print("how are you")
 # print("hello Aman Sir!!",end="\t")
 # print("how are you")
 
-print("hello","Aman","Sir")
+# print("hello","Aman","Tiwari")
 # print("hello","Aman","Sir",sep="_")
--- 01 intro_to_python/helloWorld.py
+++ 01 intro_to_python/helloWorld.py
@@ -1,5 +1,21 @@
 print("hello everyone")
 print('hello everyone')
-# print('hello everyone") #error bcz of single quote and double quote
+#print('hello everyone") #error bcz of single quote and double quote
 
     # print("Hello Aman Sir") #error bcz there is space before print
+
+
+
+
+'''
+python (core python)
+
+1.Desktop application (Thinker)
+2.Websites  (Django,Flask)
+3.Data Science and data Analysis (Numpy ,pandas,pytorch etc)
+4.embedded or robotics
+5.AI and ML 
+6.Game Development (pygames)
+7.Hacking
+
+'''
