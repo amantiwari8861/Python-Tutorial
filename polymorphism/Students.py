49bc087b371d4a4fffd9dfb46341164de460a92f
Aman Tiwari <amantiwari8861@gmail.com>
2023-07-12 09:09:55 +0000

    constructor overloading

---
+++ polymorphism/Students.py
@@ -0,0 +1,30 @@
+# overloading constructor in python
+
+class Student:
+
+    # we can't do this
+    # def __init__(self):
+    #         print("student created")
+    # def __init__(self,id):
+    #       self.id=id
+
+    def __init__(self, id=None, name=None, course=None):
+        print("student created")
+        self.id = id
+        self.name = name
+        self.course = course
+
+    def showDetails(self):
+        print("Student id is :", self.id)
+        print("Student Name is :", self.name)
+        print("Student Course is :", self.course)
+
+
+stu1 = Student()
+stu1.showDetails()
+
+stu2 = Student(101)
+stu2.showDetails()
+
+stu3 = Student(102, "Aman Tiwari", "Devops")
+stu3.showDetails()
