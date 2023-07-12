6602acf6751c00b1140e6863ac7a71d473ccec22
Aman Tiwari <amantiwari8861@gmail.com>
2023-07-12 09:10:06 +0000

    inheritance

--- 09 Inheritance/Heirarchical.py
+++ 09 Inheritance/Heirarchical.py
@@ -2,26 +2,32 @@ class Mammals:
     def mammalProperty(self):
         print("we give birth to child and we r warm blooded ")
 
+
 class cats(Mammals):
     def catsProperty(self):
         print("we walks on 4 legs and we have a tail and omnivores")
 
+
 class Human(Mammals):
     def humanProperty(self):
         print("we walk on two legs ")
+
 
 class dogs(Mammals):
     def dogsProperty(self):
         print("we walks on 4 legs and we have a tail and omnivores")
         print(" and we r human's best friend ")
 
+
 class Tiger(cats):
     def tigerProperty(self):
         print("we r big in size and very powerful and national animal of india")
 
+
 class Lion(cats):
     def lionProperty(self):
         print("we r big in size and very powerful ")
+
 
 class Leopard(cats):
     def leopardProperty(self):
@@ -29,19 +35,19 @@ class Leopard(cats):
 
 
 print("-----------------Tiger's Property--------------------")
-julie=Tiger()
+julie = Tiger()
 julie.mammalProperty()
 julie.catsProperty()
 julie.tigerProperty()
 print("-------------------Dog's property-----------------")
-tommy=dogs()
+tommy = dogs()
 tommy.mammalProperty()
 tommy.dogsProperty()
 print("--------------------Human's Property------------------")
-prashant=Human()
+prashant = Human()
 prashant.mammalProperty()
 prashant.humanProperty()
 print("----------------------Cat's Property-------------------")
-c1=cats()
+c1 = cats()
 c1.mammalProperty()
 c1.catsProperty()
